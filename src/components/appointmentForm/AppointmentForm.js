a53d347a3642a8ecb9ee3708abb0ffa33bf7dca4
Anton Vlasenko <anton.vlasenko94@gmail.com>
2022-10-11 18:53:58 +0100

    Render web form to collect appointment details

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 export const AppointmentForm = ({
   contacts,
@@ -19,7 +20,66 @@ export const AppointmentForm = ({
     return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
   };
 
+  const getContactNames = () => {
+    return contacts.map((contact) => contact.name);
+  }
+
   return (
-    AppointmentForm
+    <form onSubmit={handleSubmit}>
+      {/* Appointment Title input */}
+      <label>
+        <input 
+          name="title"
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          placeholder="Title"
+        />
+      </label>
+      <br />
+
+      {/* Add props to ContactPicker component */}
+      <label>
+        <ContactPicker
+          name="contact"
+          value={contact}
+          contacts={getContactNames()}
+          onChange={(e) => setContact(e.target.value)}
+          placeholder="Appointment with: "
+        />
+      </label>
+      <br />
+
+      {/* Date input */}
+      <label>
+        <input
+          name="date"
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+
+      {/* Time input */}
+      <label>
+        <input
+          name="time"
+          type="time"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+
+      {/* Submit */}
+      <input
+        type="submit"
+        value="Add Appointment"
+      />
+    </form>
   );
 };
