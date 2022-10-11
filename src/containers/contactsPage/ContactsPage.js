f70135488bd171ff9bf956f08c3367331cd3fcdf
Anton Vlasenko <anton.vlasenko94@gmail.com>
2022-10-11 16:35:17 +0100

    Add contacts and list contacts in ContactsPage.js

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,32 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ContactForm } from "../../components/contactForm/ContactForm";
+import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+// Extract contacts and addContact props
+export const ContactsPage = ({ contacts, addContact }) => {
+  // Define state variables
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [duplicate, setDuplicate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
+    // Add contact info and clear data if contact name is not a duplicate
+    if (!duplicate) {
+      addContact(name, number, email);
+      setName("");
+      setNumber("");
+      setEmail("");
+    }
   };
 
-  /*
-  Using hooks, check for contact name in the 
-  contacts array variable in props
-  */
+  // useEffect hook that checks if the contact name is a duplicate 
+  useEffect(() => {
+    const nameIsDuplicate = () => {
+      const nameFound = contacts.find((contact) => contact.name === name);
+      if (nameFound !== undefined) {
+        return true;
+      }
+      return false;
+    };
+
+    if (nameIsDuplicate()) {
+      setDuplicate(true);
+    } else {
+      setDuplicate(false);
+    }
+  }, [name, contacts, duplicate])
 
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        {/* Notify user if contact name is a duplicate */}
+        <h2>Add Contact {duplicate ? " - This contact name already exists" : ""}</h2>
+
+        {/* Render ContactForm with props */}
+        <ContactForm 
+          name={name}
+          setName={setName}
+          number={number}
+          setNumber={setNumber}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        {/* Render TileList with props */}
+        <TileList 
+          tiles={contacts} 
+        />
       </section>
     </div>
   );
