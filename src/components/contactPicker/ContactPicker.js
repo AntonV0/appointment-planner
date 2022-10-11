2538ecb39d4bedb0aff7d12a8b7633dc5bc7342c
Anton Vlasenko <anton.vlasenko94@gmail.com>
2022-10-11 19:24:56 +0100

    Implement contact picker within appointment form

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-export const ContactPicker = () => {
+// Extract contacts array, contact names, and onChange callback events
+export const ContactPicker = ({ name, onChange, contacts }) => {
   return (
-    ContactPicker
+    // Render select element with the onChange attribute set to the callback function 
+    <select name={name} onChange={onChange}>
+      {/* Default option element */}
+      <option value={""} key={-1} selected="selected">
+        No Contact Selected
+      </option>
+      {/* Iteratively add option elements using contact names from contacts array */}
+      {contacts.map((contact) => {
+        return (
+          <option value={contact} key={contact}>
+            {contact}
+          </option>
+        );
+      })}
+    </select>
   );
 };
